68a62629246bdaa1451296f7df916c75acc4e699
7rans <transfire@gmail.com>
2013-02-10 11:18:06 -0500

    Get testing config in order. [admin]

--- task/fire.rb
+++ task/fire.rb
@@ -1,12 +1,20 @@
 #!/usr/bin/env ruby
+ignore 'pkg', 'log'
 
-rule 'test/**/test_*.rb' do |files|
+state :need_manifest? do
+  ! system 'mast --quiet --recent'
+end
+
+rule need_manifest? do
+  sh 'mast -u'
+end
+
+rule 'test/test_*.rb' do |files|
   require 'rubytest'
-  $:.unshift('lib') 
-  runner = Test::Runner.new do |run|
+  Test.run do |run|
+    $:.unshift('lib') 
     run.files << files
   end
-  runner.run
 end
 
 desc "Run all unit tests with coverage report."
@@ -15,17 +23,15 @@ task 'test' do
   #require 'citron'
   #require 'ae'
 
-  $:.unshift('lib') 
-
-  runner = Test::Runner.new do |run|
-    run.files << 'test/**/test_*.rb'
-  end
-
   require 'simplecov'
+  SimpleCov.command_name "test:all"
   SimpleCov.start do
     coverage_dir 'log/coverage'
   end
 
-  runner.run
+  Test.run do |run|
+    $:.unshift('lib') 
+    run.files << 'test/test_*.rb'
+  end
 end
 
--- test/helper.rb
+++ test/helper.rb
@@ -1,32 +1,5 @@
-#require 'microtest/testunit'
-#require 'microtest/assertions'
-#require 'test/fixtures/multiplex'
-
 require 'citron'
 require 'ae'
 
-if ENV['p'] == 'cov'
-  require 'simplecov'
-  SimpleCov.start do
-    coverage_dir 'log/coverage'
-  end
-end
-
 require 'tomparse'
-
-#module TomParse
-#  class Test < ::Test::Unit::TestCase
-#    def self.test(name, &block)
-#      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
-#    end
-#
-#    def default_test
-#    end
-#
-#    def fixture(name)
-#      @fixtures ||= {}
-#      @fixtures[name] ||= File.read("test/fixtures/#{name}.rb")
-#    end
-#  end
-#end
 
